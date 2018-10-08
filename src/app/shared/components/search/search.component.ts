bf2cef383e8201435e1195fa0e6627632639dc92
Henrique Santiago <henriquesantiagofranco@gmail.com>
2018-10-08 16:47:24 -0300

    Restrição de endereços somente do Brasil

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { element } from 'protractor';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, DoCheck } from '@angular/core';
 import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
 import { AddressComponent } from 'ngx-google-places-autocomplete/objects/addressComponent';
@@ -11,20 +11,20 @@ import { ComponentRestrictions } from 'ngx-google-places-autocomplete/objects/op
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, DoCheck {
 
   @ViewChild("placesRef") placesRef : GooglePlaceDirective;
 
   zipCode:number;
 
+  //Restrição de endereços somente do Brasil
+  options = {
+    componentRestrictions: {country: "BR"}
+  };
+
   constructor() { }
 
   ngOnInit() {
-    this.placesRef.options.componentRestrictions = new ComponentRestrictions({
-      country: "BR"
-    });
-
-    this.placesRef.reset();
   }
 
   public handleAddressChange(address: Address) {
